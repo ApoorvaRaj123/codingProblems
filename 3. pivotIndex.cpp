bb5ac4653e24859d10367a17d8203082e63953dc
ApoorvaRaj123 <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-01-20 13:42:26 +0530

    Update 3. pivotIndex.cpp
    Give better solution to the problem

--- 3. pivotIndex.cpp
+++ 3. pivotIndex.cpp
@@ -1,46 +1,26 @@
 class Solution {
 public:
 
-    int leftSum(vector<int>& arr, int k){
-        int ls = 0;
-        for(int i = 0; i < k; i++){
-            ls += arr[i];
-        }
-        return ls;
+
+int pivotIndex(vector<int>& nums) {
+        
+    int rightSum = 0; 
+    int leftSum = 0;
+
+    for(int i = 0; i < nums.size(); i++){
+        rightSum += nums[i];
     }
+    
+    for(int i = 0; i < nums.size(); i++){
+        rightSum -= nums[i];
 
-    int rightSum(vector<int>& arr, int k){
-        int rs = 0;
-        for(int i = k+1; i <= arr.size()-1; i++){
-            rs += arr[i];
+        if(leftSum == rightSum){
+            return i;
         }
-        return rs;
-    }
-
-
-
-
-    int pivotIndex(vector<int>& nums) {
-        int s = 0;
-        int e = nums.size() - 1;
-        int mid = s + (e-s)/2;
-        int ans = -1;
-
-        while(s<=e){
-
-            if(leftSum(nums, mid) == rightSum(nums, mid)){
-                ans = mid;
-                return ans;
-            }
-
-            else if(leftSum(nums, mid) < rightSum(nums, mid)){
-                s = mid + 1;
-            }
-            else if(leftSum(nums, mid) > rightSum(nums, mid)){
-                e = mid - 1;
-            }
-            mid = s + (e-s)/2;
+        else{
+            leftSum += nums[i];
         }
-        return ans;
     }
+    return -1;
+}
 };

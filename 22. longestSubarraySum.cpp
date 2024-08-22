c67642c832942ec49cc2bc631f8b4ff2d24e3bee
ApoorvaRaj123 <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-08-22 21:49:18 +0530

    Add 22. longestSubarraySum.cpp

---
+++ 22. longestSubarraySum.cpp
@@ -0,0 +1,13 @@
+
+//          Brute Force Approach
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(vector<int>& a) {
+    int maxLen = 0;
+    unordered_map<int, int> sumIndexMap;
+    int sum = 0;
+
+    for (int i = 0; i < a.size(); i++) {
+        sum += a[i];

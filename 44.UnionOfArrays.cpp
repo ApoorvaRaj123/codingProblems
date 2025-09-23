9a7d99553c16db3e9f9982f4b62865f0067ed9f3
Apoorva Raj <99551949+ApoorvaRaj123@users.noreply.github.com>
2025-09-23 22:54:41 +0530

    Create 44.UnionOfArrays.cpp

---
+++ 44.UnionOfArrays.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> findUnion(vector<int>& a, vector<int>& b) {
+    vector<int> res;
+  
+  	for(int i = 0; i < a.size(); i++){
+      
+      	// check if the element is already 
+        // in the result to avoid duplicates
+        int j;
+        for (j = 0; j < res.size(); j++) {
+            if (res[j] == a[i]) 
+                break;
+        }
+        if (j == res.size())
+            res.push_back(a[i]);
+  	}
+
+
+  	for(int i = 0; i < b.size(); i++){
+      
+      	// check if the element is already 
+        // in the result to avoid duplicates
+        int j;
+        for (j = 0; j < res.size(); j++) {
+            if (res[j] == b[i]) 
+              break;
+        }
+        if (j == res.size())
+            res.push_back(b[i]);
+  	}
+    
+  	sort(res.begin(), res.end());
+    return res;
+}
+
+int main() {
+    vector<int> a = {1, 1, 2, 2, 2, 4};
+    vector<int> b = {2, 2, 4, 4};
+
+    vector<int> res = findUnion(a, b);
+  
+    for (int i = 0; i < res.size(); i++) 
+        cout << res[i] << " ";
+
+    return 0;
+}

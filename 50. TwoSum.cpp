2100915a2960d6bce80cecf2a6db33f3640e46ae
Apoorva Raj <99551949+ApoorvaRaj123@users.noreply.github.com>
2025-10-14 20:50:48 +0530

    Update 50. TwoSum.cpp
    Add better solution

--- 50. TwoSum.cpp
+++ 50. TwoSum.cpp
@@ -15,3 +15,24 @@ public:
         return ans;
     }
 };
+
+
+// Better solution using Hash Map
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        map<int, int> mpp;
+
+       for(int i = 0; i<nums.size(); i++){
+        int extra = target - nums[i];
+
+        if(mpp.find(extra) != mpp.end()){
+            return {mpp[extra], i};
+        }
+        mpp[nums[i]] = i;
+       }
+       return {-1, -1};
+    }
+};

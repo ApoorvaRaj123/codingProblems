f17e3754f9404b53d52b35a800cdcc4c449af272
Apoorva Raj <99551949+ApoorvaRaj123@users.noreply.github.com>
2025-10-14 21:09:58 +0530

    Update 50. TwoSum.cpp

--- 50. TwoSum.cpp
+++ 50. TwoSum.cpp
@@ -36,3 +36,25 @@ public:
        return {-1, -1};
     }
 };
+
+
+// Optimal using two pointers if it is asked to return only "YES/NO"
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        sort(nums.begin(), nums.end());
+        while(left < right){
+            int sum = nums[left] + nums[right];
+            if(sum == target){
+                return "YES";
+            }
+            else if(sum < target){
+                left++;
+            }
+            else right--;
+        }
+    return -1;
+    }
+};

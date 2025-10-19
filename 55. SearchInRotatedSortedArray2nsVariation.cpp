586b4b59ab3b27a542ac05bfb133ce1f59697cdf
Apoorva Raj <99551949+ApoorvaRaj123@users.noreply.github.com>
2025-10-19 23:05:26 +0530

    Create 55. SearchInRotatedSortedArray2nsVariation.cpp
    Add solution to the second, harder variation of, 54th problem.

---
+++ 55. SearchInRotatedSortedArray2nsVariation.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+
+        while(left <= right){
+            if(nums[left] == nums[right]){
+                if(nums[left] == target){
+                    return true;
+                }
+                left ++;
+                right --;
+                continue;
+            }
+            int mid = left + (right - left)/2;
+
+            if(nums[mid] == target){
+                return true;
+            }
+
+            if(nums[mid] >= nums[left]){
+                if(nums[left] <= target && target <= nums[mid]){
+                    right = mid - 1;
+                }
+                else{
+                    left = mid + 1;
+                }
+            }
+            else{
+                if(nums[mid] <= target && target <= nums[right]){
+                    left = mid + 1;
+                }
+                else{
+                    right = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+};

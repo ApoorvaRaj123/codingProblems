7536c2036d578bbe85fdcb91e459c20c46cbbbbf
Apoorva Raj <99551949+ApoorvaRaj123@users.noreply.github.com>
2026-06-07 11:52:37 +0530

    Update 85. MajorityElementRevisit.cpp
    Added optimal solution with thinking process

--- 85. MajorityElementRevisit.cpp
+++ 85. MajorityElementRevisit.cpp
@@ -21,3 +21,29 @@ public:
         return ans;
     }
 };
+
+// OPTIMAL Solution : at least one majority element is there.
+// So, Simlpe make a count and increase it for similar value in array.
+// As soon as element changes, decrease the count.
+// If it reaches zeor, renew the count to one and update the answer element.
+// The majority element will AT LEAST, have a count of 1 remaining.
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int  ans, count = 0;
+        for(int i = 0; i <= nums.size() - 1; i++){
+            if(count == 0){
+                count = 1;
+                ans = nums[i];
+            }
+            else if (ans == nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return ans;
+    }
+};
